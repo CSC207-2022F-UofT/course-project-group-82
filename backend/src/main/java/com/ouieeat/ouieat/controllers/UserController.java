d15cda20b63847a5ac4e798ca27a4d3a091fa3d7
Arihant Bapna <greatbapna@gmail.com>
2022-10-27 14:24:55 -0400

    Change Register/Login to Post routes

--- backend/src/main/java/com/ouieeat/ouieat/controllers/UserController.java
+++ backend/src/main/java/com/ouieeat/ouieat/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.ouieeat.ouieat.controllers;
 
 import com.ouieeat.ouieat.implementation.UserImplementation;
 import com.ouieeat.ouieat.models.User;
+import com.ouieeat.ouieat.models.UserLogin;
 import com.ouieeat.ouieat.repository.UserRepository;
 import com.ouieeat.ouieat.requests.UserRequests;
 import com.ouieeat.ouieat.responses.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -20,35 +20,25 @@ public class UserController {
     this.userRepository = userRepository;
   }
 
-  @GetMapping("/register")
-  public String register(
-    @RequestParam(value = "firstName") String firstName,
-    @RequestParam(value = "lastName") String lastName,
-    @RequestParam(value = "profilePictureLink") String profilePictureLink,
-    @RequestParam(value = "username") String username,
-    @RequestParam(value = "password") String password,
-    @RequestParam(value = "email") String email
-  ) {
-    User newUser = new User(
-      firstName,
-      lastName,
-      profilePictureLink,
-      username,
-      password,
-      email
-    );
+  @PostMapping(
+    path = "/register",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public String registerPost(@RequestBody User newUser) {
     return UserRequests.doRegister(userRepository, newUser);
   }
 
-  @GetMapping("/login")
-  public String login(
-    @RequestParam(value = "username") String username,
-    @RequestParam(value = "password") String password
-  ) {
+  @PostMapping(
+    path = "/login",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public String login(@RequestBody UserLogin userLogin) {
     Response response = UserImplementation.loginUser(
       userRepository,
-      username,
-      password
+      userLogin.getUsername(),
+      userLogin.getPassword()
     );
     return response.getJsonString();
   }
