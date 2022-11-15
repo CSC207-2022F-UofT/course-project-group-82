9c8552d988f278c67134772db8d93318a04e4f9a
Ian Huang <iahuang@users.noreply.github.com>
2022-11-14 22:24:36 -0500

    Fix MIME type of root page

--- backend/src/main/java/com/ouieat/controllers/RootController.java
+++ backend/src/main/java/com/ouieat/controllers/RootController.java
@@ -1,14 +1,16 @@
 package com.ouieat.controllers;
 
 import com.ouieat.responses.RootResponses;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
-  @GetMapping("/")
-  public String any() {
-    return RootResponses.serviceRunning().getJsonString();
-  }
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String any() {
+        return RootResponses.serviceRunning().getJsonString();
+    }
 }
