756241ae16505b428f7243d8b9d9219b685391b1
hongzipkim <hongzipkim03@gmail.com>
2022-11-17 23:38:23 -0500

    Changes made on 11/16/2022
    Creating the input parameters for FirstName, LastName, Username, Password, and set a background image for the tab

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,39 +1,86 @@
 import React from "react";
-import { View, SafeAreaView, StyleSheet, TextInput, Text} from "react-native";
+import {View, SafeAreaView, StyleSheet, TextInput, Text, ImageBackground, Button} from "react-native";
+import BackgroundImage from "./assets/userprofilebackground.png";
+import {AntDesign} from '@expo/vector-icons';
 
 export const UserProfilePage = () => {
+    const [username, onChangeUsername] = React.useState<string>();
     const [text, onChangeFirstName] = React.useState<string>();
     const [text2, onChangeLastName] = React.useState<string>();
+    const [text3, onChangeEmail] = React.useState<string>();
+    const [text4, onChangePassword] = React.useState<string>();
+
 
     return (
-        <SafeAreaView>
-            <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Text>First Name</Text>
-                <TextInput
-                    placeholder = {"First Name"}
-                    style={styles.input}
-                    onChangeText={onChangeFirstName}
-                    value={text}
-                />
-            </View>
-            <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Text>Last Name</Text>
-                <TextInput
-                    placeholder = {"Last Name"}
-                    style={styles.input}
-                    onChangeText={onChangeLastName}
-                    value={text2}
-                />
-            </View>
-        </SafeAreaView>
+            <ImageBackground
+                source={BackgroundImage}
+                style={styles.container}>
+                <SafeAreaView style={{alignItems: "center"}}>
+                    <Text style={{fontSize: 25}}>
+                        Edit Profile
+                    </Text>
+                    <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <AntDesign name="idcard" size={24} color="black" />
+                        <TextInput
+                            placeholder = {"Username"}
+                            style={styles.input}
+                            onChangeText={onChangeUsername}
+                            value={username}
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <AntDesign name="user" size={24} color="black" />
+                        <TextInput
+                            placeholder = {"First Name"}
+                            style={styles.input}
+                            onChangeText={onChangeFirstName}
+                            value={text}
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <AntDesign name="user" size={24} color="black" />
+                        <TextInput
+                            placeholder = {"Last Name"}
+                            style={styles.input}
+                            onChangeText={onChangeLastName}
+                            value={text2}
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <AntDesign name="mail" size={24} color="black" />
+                        <TextInput
+                            placeholder = {"Email"}
+                            style={styles.input}
+                            onChangeText={onChangeEmail}
+                            value={text3}
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <AntDesign name="lock1" size={24} color="black" />
+                        <TextInput
+                            placeholder = {"Password"}
+                            style={styles.input}
+                            onChangeText={onChangePassword}
+                            value={text4}
+                        />
+                    </View>
+                </SafeAreaView>
+            </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%'
+    },
     input: {
         height: 40,
         margin: 12,
+        width: "50%",
         borderWidth: 1,
+        borderRadius: 10,
         padding: 10,
     },
 });
