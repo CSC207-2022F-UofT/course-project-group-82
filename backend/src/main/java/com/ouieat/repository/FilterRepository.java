a97861ff901c056cea528dcc201e29f0c0793342
Tanvipatel9 <tani.patel@mail.utoronto.ca>
2022-11-20 12:52:02 -0500

    filterRepository interface created

--- backend/src/main/java/com/ouieat/repository/FilterRepository.java
+++ backend/src/main/java/com/ouieat/repository/FilterRepository.java
@@ -1,6 +1,4 @@
 package com.ouieat.repository;
-
-
 import ch.qos.logback.core.filter.Filter;
 import org.apache.catalina.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,19 +7,13 @@ import org.springframework.data.mongodb.repository.Query;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public abstract class FilterRepository implements MongoRepository<User, String> {
+public interface FilterRepository extends MongoRepository<User, String> {
     @Query("{distance:  '?0'}")
-    ArrayList<Filter> findRestaurantByDistance(float distance) {
-        return null;
-    }
+    ArrayList<Filter> findRestaurantByDistance(String distance);
     @Query("{cuisine:  '?0'}")
-    ArrayList<Filter> findRestaurantByCuisine(String cuisine) {
-        return null;
-    }
+    ArrayList<Filter> findRestaurantByCuisine(String cuisine);
     @Query("{priceRange:  '?0'}")
-    ArrayList<Filter> findRestaurantByPriceRange(Array[] PriceRange) {
-        return null;
-    }
+    ArrayList<Filter> findRestaurantByPriceRange(Array[] PriceRange);
 
 
 }
