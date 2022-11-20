c699a58033b2680a7042a905ec21f97071b219f7
Arlene Wang <wang.s.arlene@gmail.com>
2022-11-20 00:02:44 -0500

    setup RecommendRestaurant page, controller and model written

---
+++ frontend/src/pages/RecommendRestaurant/RecommendRestaurantPageModel.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import {RecommendRestaurantPageController} from "./RecommendRestaurantPageController";
+export function RecommendRestaurantPageModel(props: {navigation: any}) {
+    const [restaurantName, setRestaurantName] = useState<string>("");
+    const [opinion, setOpinion] = useState<boolean>(true);
+    const [tags, setTags] = useState<Array<string>>([]);  // CHECK initial state
+    const [errorVisible, setErrorVisible] = useState<boolean>(false);
+    const [errors, setErrors] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const controllerProps = {
+        restaurantName,
+        setRestaurantName,
+        opinion,
+        setOpinion,
+        tags,
+        setTags,
+        errorVisible,
+        setErrorVisible,
+        errors,
+        setErrors,
+        loading,
+        setLoading,
+    };
+
+    return <RecommendRestaurantPageController {...controllerProps}></RecommendRestaurantPageController>;
+}
